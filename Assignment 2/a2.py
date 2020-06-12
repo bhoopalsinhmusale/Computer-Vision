b682cb2d9605f7eaf293a5a3e11985f2b6691dbd
Bhoopalsinh Musale <bhoopalsinhmusale@gmail.com>
2020-06-12 18:58:13 -0400

    MI half done

--- Assignment 2/a2.py
+++ Assignment 2/a2.py
@@ -6,35 +6,6 @@ import cv2
 from PIL import Image
 
 
-def joint_histogram(A, B, binA, binB):
-
-    A = A.ravel()
-    B = B.ravel()
-    A2 = A.copy()
-    B2 = B.copy()
-
-    # assign bins
-    for i in range(1, len(binA)):
-        Ai = np.where(np.bitwise_and(A > binA[i-1], A <= binA[i]))
-        A2[Ai] = i-1
-    for i in range(1, len(binB)):
-        Bi = np.where(np.bitwise_and(B > binB[i-1], B <= binB[i]))
-        B2[Bi] = i-1
-    JH = np.zeros((len(binA)-1, len(binB)-1))
-    # calculate joint histogram
-    for i in range(len(A)):
-        JH[A2[i], B2[i]] += 1
-    # calculate histogram for A
-    HA = np.zeros(len(binA)-1)
-    for i in range(len(A)):
-        HA[A2[i]] += 1
-    # calculate histogram for B
-    HB = np.zeros(len(binB)-1)
-    for i in range(len(B)):
-        HB[B2[i]] += 1
-    return JH, HA, HB
-
-
 def joint_histogram(I, J, num_bins=16, minmax_range=None):
 
     if I.shape != J.shape:
@@ -62,9 +33,9 @@ def joint_histogram(I, J, num_bins=16, minmax_range=None):
     for k in range(n):
         p[I[k], J[k]] = p[I[k], J[k]] + 1
     print("aaaaaaaa%", np.sum(p))
-    # p = p/n
-
-    return p
+    norma_jhist = p/n
+    jhist = p
+    return jhist, norma_jhist
 
 
 def ssd(A, B):
@@ -91,6 +62,29 @@ def pearson_correlation(x, y):
         return correlation
 
 
+def MI(I, J):
+    n = I.shape[0]
+    _, norm_jhist = joint_histogram(I, J)
+    print("Normalized Joint Histogram={}".format(np.sum(norm_jhist)))
+    # compute marginal histograms
+    jh = jh + EPS
+    sh = np.sum(jh)
+    jh = jh / sh
+    s1 = np.sum(jh, axis=0).reshape((-1, jh.shape[0]))
+    s2 = np.sum(jh, axis=1).reshape((jh.shape[1], -1))
+
+    # Normalised Mutual Information of:
+    # Studholme,  jhill & jhawkes (1998).
+    # "A normalized entropy measure of 3-D medical image alignment".
+    # in Proc. Medical Imaging 1998, vol. 3338, San Diego, CA, pp. 132-143.
+    if normalized:
+        mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2)))
+              / np.sum(jh * np.log(jh))) - 1
+    else:
+        mi = (np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1))
+              - np.sum(s2 * np.log(s2)))
+
+
 if __name__ == "__main__":
     '''for i in range(2, 7):
         im_frame = Image.open('I{}.jpg'.format(i))
@@ -102,7 +96,7 @@ if __name__ == "__main__":
         print(J.shape)
 
         fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
-        my_joint_hist = joint_histogram(I, J, 50)
+        my_joint_hist,_ = joint_histogram(I, J, 50)
         jhist = np.histogram2d(np.ravel(I), np.ravel(J), 50)
         ax1.imshow(np.log(my_joint_hist))
         ax1.set_title("My joint hist")
@@ -127,5 +121,7 @@ if __name__ == "__main__":
     #ssd = ssd(I, J)
     # print(ssd)
 
-    corr = pearson_correlation(I, J)
-    print(corr)
+    #corr = pearson_correlation(I, J)
+    # print(corr)
+
+    MI(I, J)
