c059177a700edcda58709bbed0220d5440417ba9
Bhoopalsinh Musale <bhoopalsinhmusale@gmail.com>
2020-06-23 23:40:16 -0400

    final build 3

--- Assignment 2/a2.py
+++ Assignment 2/a2.py
@@ -48,11 +48,12 @@ def joint_histogram(I, J, num_bins=16):
 # Part 2-A
 def ssd(A, B):
     dif = A.ravel() - B.ravel()
-    return np.dot(dif, dif)
+    return np.sum(np.square(A - B))
 
 
 # Part 2-B
 def pearson_correlation(x, y):
+    # used from data mining course
     x = x.ravel()
     y = y.ravel()
     n = len(x)
@@ -234,13 +235,13 @@ def affine_transform(slice=0, theta=0, omega=0, phi=0, dx=0, dy=0, dz=0):
                    [-0.6426, -1.7985, -1.6285, 0.7404],
                    [0, 0, 0, 1.0000]])
 
-    print(np.dot(inv(M1).T, T.T).T)
+    # print(np.dot(inv(M1).T, T.T).T)
 
 
 # Part 4-A
 def translation(I, p=0, q=0):
-    print("P={}".format(p))
-    print("Q={}".format(q))
+    # print("P={}".format(p))
+    # print("Q={}".format(q))
 
     img = imread(I)
     # img = cers[100:300, 100:300]
@@ -257,8 +258,8 @@ def translation(I, p=0, q=0):
                   [0, 0, 1]]).T
 
     final_img = np.dot(A, T)
-    print("asssssss")
-    print(final_img.T[0, :].shape)
+    # print("asssssss")
+    # print(final_img.T[0, :].shape)
     f = interp2d(final_img.T[0, :], final_img.T[1, :],
                  img, kind='cubic', fill_value=0)
     znew = f(x, y)
@@ -274,9 +275,33 @@ def translation(I, p=0, q=0):
     return znew
 
 
+# Part 4-B
+def regis_trans(i1, i2):
+    ssds = []
+    p = q = 0
+    i1 = imread(i1)
+    stepsize = 0.000005
+    n_iter = 100
+
+    for _ in range(n_iter):
+        translated_i2 = translation(i2, p, q)
+        sub = translated_i2 - i1
+        ssds.append(ssd(i1, translated_i2))
+
+        # compute gradient
+        x_grad, y_grad = np.gradient(translated_i2)
+        ssd_p = 2 * np.sum(np.multiply(sub, x_grad))
+        ssd_q = 2 * np.sum(np.multiply(sub, y_grad))
+
+        p -= stepsize * ssd_p
+        q -= stepsize * ssd_q
+
+    return ssds
+
+
 # Part 4-C
-def rotation(I, theta=45):
-    img = imread(I)
+def rotation(img, theta=45):
+    #img = imread(I)
     # img = img[100:300, 100:300]
     height, width = img.shape
 
@@ -309,9 +334,73 @@ def rotation(I, theta=45):
     return znew
 
 
-if __name__ == "__main__":
+# Part 4-D
+def ssd_rotat(i1, i2):
+
+    i1 = imread(i1)
+    i2 = imread(i2)
+    ssds = []
+    theta = 0
+    stepsize = 0.000005
+    n_iter = 100
+    for _ in range(n_iter):
+        # move i2 and save SSD
+        rotated_i2 = rotation(i2, theta)
+        sub = rotated_i2 - i1
+        ssds.append(ssd(i1, rotated_i2))
+
+        # compute gradient
+        x_grad, y_grad = np.gradient(rotated_i2)
+
+        # print(i2)
+        # print(i2.shape)
+        x, y = np.mgrid[0:i2.shape[0], 0:i2.shape[1]]
+        angle = np.deg2rad(theta)
+        s, c = np.sin(angle), np.cos(angle)
+        ssd_theta = 2 * np.sum(np.multiply(sub,
+                                           np.multiply(x_grad, -(x * s + y * c)) +
+                                           np.multiply(y_grad, x * c - y * s)))
+        # print("ssd_theta: ", ssd_theta)
+        # print("theta: ", theta)
+
+        # update parameters
+        theta -= stepsize * ssd_theta * 0.01
+
+    return ssds
+
+
+# Part 4-E
+def rigid_transform(i, p, q, theta):
+    """Return a new image corresponding to image i both translated and rotated"""
+    len_x, len_y = i.shape
+    x, y = np.mgrid[0:len_x, 0:len_y]
+
+    t = np.array([[1, 0, p],
+                  [0, 1, q],
+                  [0, 0, 1]])
+
+    angle = np.deg2rad(theta)
+    s, c = np.sin(angle), np.cos(angle)
+    r = np.array([[c, s, 0],
+                  [-s, c, 0],
+                  [0, 0, 1]])
+
+    T = t @ r
+
+    axes = np.vstack([np.ravel(x), np.ravel(y), np.ones(len_x * len_y)])
+    data_points = axes[:-1].T
+    new_axes = np.apply_along_axis(lambda col: T @ col.T, 0, axes)[:-1]
+    new_x, new_y = new_axes[0].reshape(
+        len_x, len_y), new_axes[1].reshape(len_x, len_y)
+    new_i = interpolate.griddata(data_points, np.ravel(
+        i), (new_x, new_y), method='cubic', fill_value=0)
+
+    return new_i
+
+
+def part_1():
     # Part 1(A,B,C)
-    '''img1 = imread("I1.png")
+    img1 = imread("I1.png")
     # I = np.array(img1)
     print(img1.shape)
 
@@ -335,8 +424,9 @@ if __name__ == "__main__":
     plt.title("Joint Histogram")
     plt.suptitle("Part-1\n nxp={}".format(np.sum(myjh)))
     plt.tight_layout()
-    plt.show()'''
+    plt.show()
 
+    # Following code is for testing with different pairs of images
     '''for i in range(2, 7):
         img1 = imread('I{}.jpg'.format(i))
         # img1 = imread("I1.png")
@@ -367,24 +457,30 @@ if __name__ == "__main__":
         plt.tight_layout()
         plt.show()'''
 
+
+def part_2():
     # Part 2-A,B,C,D
-    '''img1 = imread("I1.png")
+    img1 = imread("I1.png")
     img2 = imread("J1.png")
     ssd = ssd(img1, img2)
     corr = pearson_correlation(img1, img2)
     mi = MI(img1, img2)
-    print("SSD={} | CORR={} | MI={}".format(ssd, corr, mi))'''
+    print("SSD={} | CORR={} | MI={}".format(ssd, corr, mi))
 
+
+def part_3():
     # Part 3-A, B
-    '''rigid_transform(theta=90, omega=0,
+    rigid_transform(theta=90, omega=0,
                     phi=0, dx=0, dy=0, dz=0)
 
     # Part 3-C
     affine_transform(slice=-20, theta=90, omega=0,
-                     phi=0, dx=0, dy=0, dz=0)'''
+                     phi=0, dx=0, dy=0, dz=0)
 
+
+def part_4():
     # Part 4-A
-    '''img = imread("I1.png")
+    img = imread("I1.png")
     plt.subplot(1, 2, 1)
     plt.imshow(img)
     plt.title("Original")
@@ -395,10 +491,30 @@ if __name__ == "__main__":
     plt.title("Transformed")
     plt.suptitle("Part-4 A\nTranslation")
     plt.tight_layout()
-    plt.show()'''
+    plt.show()
+
+    # Part 4 B,C,D
+    images = ["BrainMRI_1.jpg", "BrainMRI_2.jpg",
+              "BrainMRI_3.jpg", "BrainMRI_4.jpg"]
+
+    # Part 4-B
+    ssd_pts = []
+    for i in range(1, len(images)):
+
+        ssd_i = regis_trans(images[0], images[i])
+        ssd_pts.append(ssd_i)
+
+    plt.subplot(1, 2, 1)
+    for i, ssd_i in enumerate(ssd_pts):
+        plt.plot(ssd_i, label="BrainMRI_{}".format(i+2))
+
+    plt.suptitle('Part 4-B\nminimizing SSD (translation)')
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
     # Part 4-C
-    img = imread("I1.png")
+    '''img = imread("I1.png")
     plt.subplot(1, 2, 1)
     plt.imshow(img)
     plt.title("Original")
@@ -409,4 +525,37 @@ if __name__ == "__main__":
     plt.title("Transformed")
     plt.suptitle("Part-4 C\nRotation")
     plt.tight_layout()
-    plt.show()
+    plt.show()'''
+
+    # Part 4-D
+    '''ssd_pts = []
+    for i in range(1, len(images)):
+        ssd_i = ssd_rotat(
+            images[0], images[i])
+        ssd_pts.append(ssd_i)
+
+    plt.subplot(1, 2, 1)
+    for i, ssd_i in enumerate(ssd_pts):
+        plt.plot(ssd_i, label="BrainMRI_{}".format(i+2))
+
+    plt.suptitle('Part 4-D\nminimizing SSD (rotations)')
+    plt.legend()'
+    plt.show()'''
+
+    # Part 4-E
+
+
+if __name__ == "__main__":
+
+    # part_1()
+
+    # part_2()
+
+    # part_3()
+
+    part_4()
+
+    """Describe the SSD curve, is it strictly decreasing, and if not, why?
+       discuss the quality of your registration
+       stepsize or local optimum
+    """
