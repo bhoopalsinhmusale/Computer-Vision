7f5a707f3ae23a63ba0c73c953f85dddfb2cb63b
Divya <Bhoopalsinh Musale>
2020-07-15 09:53:29 -0600

    really started

--- Assignment-4/a4.py
+++ Assignment-4/a4.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.image import extract_patches_2d
 import matplotlib.image as mpimg
 import os
+import matplotlib.patches as patches
 
 
 def part_1():
-    img = mpimg.imread("t1.png")
-    print(img.shape)
-    patch = extract_patches_2d(img, patch_size=(256, 256), max_patches=1)
-    print(patch.shape)
-    plt.subplot(1, 1, 1)
-    plt.imshow(patch[0], cmap='gray')
-    plt.colorbar()
+    images = ["t1.png",  "t1_v2.png",  "t1_v3.png", " t2.png"]
+    img = mpimg.imread(images[0])
+    fig, ax = plt.subplots(1)
+    ax.imshow(img, cmap='gray')
+    ax.set_title("{}".format(images[0]))
+
+    rect = patches.Rectangle(
+        (10, 90), 50, 50, linewidth=1, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+
     plt.show()
 
 
