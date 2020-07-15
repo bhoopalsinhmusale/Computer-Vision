68741b52d58fcc5b30f5626d72935a195b8fe659
Divya <Bhoopalsinh Musale>
2020-07-15 08:32:45 -0600

    part 1

--- Assignment-4/a4.py
+++ Assignment-4/a4.py
@@ -1,20 +1,22 @@
-from sklearn.datasets import load_sample_image
-from sklearn.feature_extraction import image
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.image import extract_patches_2d
+import matplotlib.image as mpimg
 import os
 
 
 def part_1():
-    # Use the array data from the first image in this dataset:
-    one_image = load_sample_image(
-        "/home/divya/Desktop/Computer-Vision/Assignment-4/t1.png")
-    print('Image shape: {}'.format(one_image.shape))
-
-    patches = image.extract_patches_2d(one_image, (2, 2))
-    print('Patches shape: {}'.format(patches.shape))
-
-    # Here are just two of these patches:
-    print(patches[1])
+    img = mpimg.imread("t1.png")
+    print(img.shape)
+    patch = extract_patches_2d(img, patch_size=(256, 256), max_patches=1)
+    print(patch.shape)
+    plt.subplot(1, 1, 1)
+    plt.imshow(patch[0], cmap='gray')
+    plt.colorbar()
+    plt.show()
 
 
 if __name__ == "__main__":
+    os.system("clear")
+
     part_1()
